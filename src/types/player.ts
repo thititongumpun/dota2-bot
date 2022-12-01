136a03fdfce0009c0b79012a1bf5a6384d32be24
thititongumpun <thiti180536@gmail.com>
2022-12-01 17:27:05 +0700

    add more players

--- src/types/player.ts
+++ src/types/player.ts
@@ -1,13 +1,13 @@
 export const getPlayerId = (playerName: string): number => {
   let playerId: number = 0;
   switch (playerName.trim().replace(/ /g, "")) {
-    case "เดอะแจค":
+    case "เดอะแจค" || "เดอะแจ็ค":
       playerId = 304906167;
       break;
-    case "พี่ชาย":
+    case "พี่ชาย" || "แอสเซนตี้ฝ่ายเคลม":
       playerId = 233284542;
       break;
-    case "แครี่ที่ดีที่สุดในยุคนี้":
+    case "แครี่ที่ดีที่สุดในยุคนี้" || "เดอะนอส":
       playerId = 100719034;
       break;
     case "เดอะตี๋":
