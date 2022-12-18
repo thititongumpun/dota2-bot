8a16f71ce30305809b7ce1411924f2636f305d4c
thititongumpun <thiti180536@gmail.com>
2022-12-19 01:42:34 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express, { Express } from "express";
 import morgan from "morgan";
 import compression from "compression";
+import cors, { CorsOptions } from 'cors';
 import * as dotenv from "dotenv";
 import { routes } from "./routes/index";
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 8080;
+const options: CorsOptions = {
+  origin: "*"
+}
 
 app.use(morgan("tiny"));
 app.use(compression());
+app.use(cors(options));
 app.use("/", routes);
 app.use(require("express-status-monitor")());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Application is live and listening on port ${PORT}`);
